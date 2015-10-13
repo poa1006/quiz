7ed186c98d13592fbd313e1383fbf41a6f4148ac
poa1006 <poa1006@gmail.com>
2015-10-13 11:51:20 +0800

    Write smallest_character function with Recursive and Iterative ways

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,46 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE_SIZE 32
+
+char smallest_character(char str[],char c )
+{
+
+    int l =0, r = strlen(str)-1;
+    char res = str[0];
+    while(l <=r ) {
+        int m = l + (r-l)/2;
+        if (str[m]>c) {
+            res = str[m];
+            r = m-1;
+        } else
+            l = m+1;
+    }
+    return res;
+}
 
 int main()
 {
+    FILE *fp;
+    char line[MAX_LINE_SIZE],str[MAX_LINE_SIZE];
+    int i,che=0;
+
+    fp = fopen("data.txt","r");
+
+    while (fgets(line, sizeof(line), fp)) {
+        while (line[i] != '\0')
+            i++;
+        line[i - 1] = '\0';
+
+        if(che%2==0)
+            strcpy(str,line);
+        else
+            printf("%c\n", smallest_character(str,line[0]));
+
+        i = 0,che++;
+    }
+
+    fclose(fp);
     return 0;
 }
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,41 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <string.h>
 
+#define MAX_LINE_SIZE 32
+
+
+char smallest_character(char str[],char c,int i)
+{
+    if(str[i]>c)
+        return str[i];
+    else if(strlen(str)-1==i)
+        return str[0];
+    else
+        return smallest_character(str,c,++i);
+
+}
 int main()
 {
+    FILE *fp;
+    char line[MAX_LINE_SIZE],str[MAX_LINE_SIZE];
+    int i,che=0;
+
+    fp = fopen("data.txt","r");
+
+    while (fgets(line, sizeof(line), fp)) {
+        while (line[i] != '\0')
+            i++;
+        line[i - 1] = '\0';
+
+        if(che%2==0)
+            strcpy(str,line);
+        else
+            printf("%c\n", smallest_character(str,line[0],0));
+
+        i = 0,che++;
+    }
+
+    fclose(fp);
     return 0;
 }
