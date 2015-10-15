2fa9c8b9f1c16b0b347b91e7d3ee6f604cb575af
poa1006 <poa1006@gmail.com>
2015-10-15 11:31:33 +0800

    Add function in main.c to calculate execution time

--- q2/main.c
+++ q2/main.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <time.h>
 #include IMPL
 
 #define MAX_LINE_SIZE 32
+
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
 
 int main()
 {
     FILE *fp;
     char line[MAX_LINE_SIZE],str[MAX_LINE_SIZE];
     int i,che=0;
+    struct timespec start, end;
+    double cpu_time1;
 
     fp = fopen("data.txt","r");
-
+    clock_gettime(CLOCK_REALTIME, &start);
     while (fgets(line, sizeof(line), fp)) {
         while (line[i] != '\0')
             i++;
@@ -25,7 +40,10 @@ int main()
 
         i = 0,che++;
     }
-
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time1 = diff_in_second(start, end);
     fclose(fp);
+
+    printf("execution time of smallest_character() : %lf sec\n", cpu_time1);
     return 0;
 }
