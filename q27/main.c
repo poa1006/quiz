f8b0ed8bc08fa0ba272a1654cce77fa69067a852
poa1006 <poa1006@gmail.com>
2015-10-15 21:08:46 +0800

    Use Fisher-Yates shuffle to design the program

--- q27/main.c
+++ q27/main.c
@@ -1,6 +1,29 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
 
+void Fish_Yated_shuffle(int arr[],int n)
+{
+    int i,j,tmp;
+    for(i=n-1; i>0; i--) {
+        j= rand()%(i+1);
+        tmp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=tmp;
+    }
+}
 int main()
 {
+    int num[52];
+
+    for(int i=0; i<52; i++)
+        num[i]=i+1;
+
+    Fish_Yated_shuffle(num,52);
+
+    printf("After shuffling :\n");
+    for(int i=0; i<52; i++)
+        printf("%d\n",num[i]);
+
     return 0;
 }
